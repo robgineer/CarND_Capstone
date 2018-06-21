7d4c5f1ce79ae67cb3eb7a1c8fda5f00afa175da
robgineer <robgineer@gmail.com>
2018-06-21 19:26:51 +0200

    waypoint KDTree access

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -63,8 +63,8 @@ class WaypointUpdater(object):
             # (avoid potential access to undefined variables)
             if self.ego_pose != None and self.base_waypoints != None:
                 # publish closest waypoint
-                print(type(self.base_waypoints))
-                print(type(self.waypoint_KDTree))
+                rospy.loginfo(type(self.base_waypoints))
+                rospy.loginfo(type(self.waypoint_KDTree))
                 next_wpt_idx = self.get_next_waypoint_idx()
                 self.publish_nxt_waypoints(next_wpt_idx)
         # got to sleep for 20ms
